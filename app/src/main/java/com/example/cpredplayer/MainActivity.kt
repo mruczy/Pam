a2f17338a3ba715f8aa1e0a350a83649a666fdd5
mruczy <mru.michal@gazeta.pl>
2023-04-25 21:52:36 +0200

    add binding

--- app/src/main/java/com/example/cpredplayer/MainActivity.kt
+++ app/src/main/java/com/example/cpredplayer/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.cpredplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cpredplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //private val dbHandler by lazy { DBHandler(this,) }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
